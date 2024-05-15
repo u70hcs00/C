7b49c745d1e7974eb9746f0a616d7ae8b028737d
u70hcs00 <77768070+u70hcs00@users.noreply.github.com>
2024-05-15 11:59:38 +0800

    Add files via upload

--- ch12/List12_3a.cpp
+++ ch12/List12_3a.cpp
@@ -1,26 +1,26 @@
 #include<iostream>
 using namespace std;
-template <typename T>
+template <typename T, int length>
 class Array{
    private:
-      T data[5];
+      T data[length];
    public:
       void setData(int num, T d);
       T getData(int num);
 };
 
-template <typename T> 
-void Array<T>::setData(int num, T d)
+template <typename T, int length> 
+void Array<T, length>::setData(int num, T d)
 {
-	if(num < 0 || num > 4 )
+	if(num < 0 || num > length )
 		cout << "oversize¡C\n";
    else
       data[num] = d;
 }
-template <typename T> 
-T Array<T>::getData(int num)
+template <typename T, int length> 
+T Array<T, length>::getData(int num)
 {
-	if(num < 0 || num > 4 ){
+	if(num < 0 || num > length ){
 		cout << "oversize¡C\n";
 		return data[0];
 	}
@@ -31,7 +31,7 @@ T Array<T>::getData(int num)
 int main()
 {
    cout << "Create Array of type int\n";
-   Array<int> i_array;
+   Array<int, 5> i_array;
    i_array.setData(0, 80);
    i_array.setData(1, 60);
    i_array.setData(2, 58);
@@ -42,7 +42,7 @@ int main()
       cout << i_array.getData(i) << '\n';
 
    cout << "Create Arrat of type double\n";
-   Array<double> d_array;
+   Array<double, 5> d_array;
    d_array.setData(0, 35.5);
    d_array.setData(1, 45.6);
    d_array.setData(2, 26.8);
--- ch12/List12_4a.cpp
+++ ch12/List12_4a.cpp
@@ -1,26 +1,37 @@
 #include<iostream>
 using namespace std;
-template <typename T>
+template <typename T, int length>
 class Array{
    private:
-      T data[5];
+      T data[length];
    public:
       void setData(int num, T d);
       T getData(int num);
+      T getMax();
 };
 
-template <typename T> 
-void Array<T>::setData(int num, T d)
+template <typename T, int length> 
+T Array<T, length>::getMax()
 {
-	if(num < 0 || num > 4 )
+	T max = data[0];
+	for(int i = 1; i < length; i++)
+	  max = (data[i] > max) ? data[i] : max;
+    return max;
+}
+
+
+template <typename T, int length> 
+void Array<T, length>::setData(int num, T d)
+{
+	if(num < 0 || num > length )
 		cout << "oversize¡C\n";
    else
       data[num] = d;
 }
-template <typename T> 
-T Array<T>::getData(int num)
+template <typename T, int length> 
+T Array<T, length>::getData(int num)
 {
-	if(num < 0 || num > 4 ){
+	if(num < 0 || num > length ){
 		cout << "oversize¡C\n";
 		return data[0];
 	}
@@ -31,18 +42,22 @@ T Array<T>::getData(int num)
 int main()
 {
    cout << "Create Array of type int\n";
-   Array<int> i_array;
+   Array<int, 5> i_array;
    i_array.setData(0, 80);
    i_array.setData(1, 60);
    i_array.setData(2, 58);
    i_array.setData(3, 77);
    i_array.setData(4, 57);
 
+
    for(int i = 0; i < 5; i++)
       cout << i_array.getData(i) << '\n';
 
+   cout << "Max is " << i_array.getMax() << endl;
+
+
    cout << "Create Arrat of type double\n";
-   Array<double> d_array;
+   Array<double, 5> d_array;
    d_array.setData(0, 35.5);
    d_array.setData(1, 45.6);
    d_array.setData(2, 26.8);
@@ -51,5 +66,7 @@ int main()
 
    for(int j = 0; j < 5; j++)
       cout << d_array.getData(j) << '\n';
+      
+   cout << "Max is " << d_array.getMax() << endl;      
    return 0;
 }
